e5a2ce869692f723e905638ca41fe6bcdcf14d7a
LuciBurcu <luciburcu@gmail.com>
2020-01-02 20:11:22 +0200

    React first example with django

--- FrontEnd/frontend/src/Components/MainComponent.js
+++ FrontEnd/frontend/src/Components/MainComponent.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import {Container} from "@material-ui/core";
+import axiosInstance from "../Axios/AxiosInstance";
 
 
-let incrementA = "a"
+
 class MainComponent extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             name: "",
@@ -14,17 +15,27 @@ class MainComponent extends React.Component {
 
     }
 
-    onClickMyFunction = () =>{
-        incrementA = incrementA + "a";
-        this.setState({
-            name: incrementA
-        })
+    componentDidMount() {
+        let nameParam = "";
+        axiosInstance.get("products/").then(
+            (res) => {
+                res.data.forEach(element =>
+                    nameParam = nameParam + element.title
+                );
+                this.setState({name: nameParam});
+            }
+        )
+    }
+
+    onClickMyFunction = () => {
+
     };
+
 
     render() {
         return (
             <Container>
-                <Button variant="contained" color="primary" onClick = {this.onClickMyFunction}>Press me</Button>
+                <Button variant="contained" color="primary" onClick={this.onClickMyFunction}>Press me</Button>
                 <p>{this.state.name}</p>
             </Container>
         );
