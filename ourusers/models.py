fbaf3159e04c87096e85cb94cc47c92d3a5c8c05
LuciBurcu <luciburcu@gmail.com>
2020-01-03 18:49:26 +0200

    Django textfield->charfield

--- ourusers/models.py
+++ ourusers/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class OurUser (models.Model) : 
     username = models.CharField(max_length = 20)
-    password = models.TextField()
+    password = models.CharField(max_length=20)
     email = models.EmailField()
-    name = models.TextField()
+    name = models.CharField(max_length=50)
     date_of_birth = models.DateField()
     description = models.TextField()
     
--- resolutions/models.py
+++ resolutions/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from ourusers.models import OurUser
 # Create your models here.
 class Resolution(models.Model) :
-    name = models.TextField()
+    name = models.CharField(max_length=100)
     done = models.BooleanField()
     tags = models.TextField()
     id_user = models.ForeignKey(OurUser, on_delete=models.CASCADE)
