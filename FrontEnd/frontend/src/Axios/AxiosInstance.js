d351eff8b7a3e233f1901079076e2fc45159be89
Neagrau Seba <seba.ng@yahoo.com>
2020-01-03 18:26:31 +0200

    post method working

--- FrontEnd/frontend/src/Axios/AxiosInstance.js
+++ FrontEnd/frontend/src/Axios/AxiosInstance.js
@@ -2,15 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: "http://127.0.0.1:8000/",
-    headers: {
-        post: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers":
-                "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
-            "Access-Control-Allow-Methods" :  "GET, POST, PUT, DELETE, OPTIONS"
-        }
-    }
+   
 });
 
 
--- products/views.py
+++ products/views.py
@@ -10,10 +10,23 @@ def get_products(request) :
     product_list = Product.objects.all().values()
     return JsonResponse (list(product_list), safe=False)
 
-@csrf_exempt
-def save_product(request) :
-    p = Product(title = "Produs nou",description = "yas",price = "20.20")
+@csrf_exempt #todo
+def save_product(request,*args) :
+    body = json.loads(request.body)
+    p = Product(title = body["title"]["product_name"],description = "yas",price = "20.20")
+    print(body["title"]["product_name"])
     p.save()
     print(request)
-
+    print(body)
     return HttpResponse(" ")
+
+@csrf_exempt #todo
+def update_product(request,product_id):
+    p = Product.objects.get(pk = product_id)
+    p.description = "DESCRIEREA NOUA"
+    p.save()
+    return HttpResponse("e ok")
+@csrf_exempt #todo
+def delete_product(request,product_id):
+    p = Product.objects.get(pk = product_id).delete()
+    return HttpResponse("e ok")
